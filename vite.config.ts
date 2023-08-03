694f1d3fba78e7ecb3a8166a9e76d936aff9829f
Adimac93 <adimac93@gmail.com>
2023-08-03 13:12:02 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { internalIpV4 } from 'internal-ip'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [svelte()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    host: "0.0.0.0",
+    port: 5173,
     strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: await internalIpV4(),
+      port: 5183,
+    },
   },
-  // 3. to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
 }));
